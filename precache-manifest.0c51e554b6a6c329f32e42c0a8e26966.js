fda77147326e3af8b989740ed942f8c5995d98a0
Sebastian Piaścik <sebastian.piascik97@gmail.com>
2019-11-01 12:56:15 +0100

    Updates

---
+++ precache-manifest.0c51e554b6a6c329f32e42c0a8e26966.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86a5d09edfe31f2c1285af570e21e84b",
+    "url": "/react-hooks-todo/index.html"
+  },
+  {
+    "revision": "1a214fc2b1c11ce2d2e1",
+    "url": "/react-hooks-todo/static/css/main.ab46b9cc.chunk.css"
+  },
+  {
+    "revision": "44600c7b0a2d6921746c",
+    "url": "/react-hooks-todo/static/js/2.3ab68b65.chunk.js"
+  },
+  {
+    "revision": "1a214fc2b1c11ce2d2e1",
+    "url": "/react-hooks-todo/static/js/main.9ada7884.chunk.js"
+  },
+  {
+    "revision": "f33cfdd450d3dbd84098",
+    "url": "/react-hooks-todo/static/js/runtime~main.9bdab4ff.js"
+  },
+  {
+    "revision": "2927f8de4268bae8f11c4dfe3adc72b5",
+    "url": "/react-hooks-todo/static/media/edit.2927f8de.svg"
+  },
+  {
+    "revision": "f29dfa384c0efd7b5263bb65782eb902",
+    "url": "/react-hooks-todo/static/media/home.f29dfa38.svg"
+  },
+  {
+    "revision": "887feffc881bdf9b4366eb31ac214916",
+    "url": "/react-hooks-todo/static/media/list.887feffc.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-hooks-todo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e4e7ad994ddcce69b97433e0cc4c5d5a",
+    "url": "/react-hooks-todo/static/media/plus.e4e7ad99.svg"
+  },
+  {
+    "revision": "569e0fbf5dbeddfa1ecf2115b3b1f262",
+    "url": "/react-hooks-todo/static/media/trash.569e0fbf.svg"
+  }
+]);
